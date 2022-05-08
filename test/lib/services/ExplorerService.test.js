aceed245581a0acf5914f014fbd6150d8ef82a44
PressureDraper <warmarines00@hotmail.com>
2022-05-08 01:18:11 -0500

    add: all unit tests for ExplorerService class

---
+++ test/lib/services/ExplorerService.test.js
@@ -0,0 +1,36 @@
+const Reader = require("../../../lib/utils/Reader");
+const eService = require("../../../lib/services/ExplorerService")
+
+describe("General tests for ExplorerService class", () => {
+    test("1. Reading explorers.json file", () => {
+        const explorers = Reader.readJsonFile("./app/explorers.json");
+        expect(explorers).not.toBeUndefined();
+    });
+    test("2. Getting all node users from explorers.json", () => {
+        const explorers = Reader.readJsonFile("./app/explorers.json");
+        const result = eService.filterByMission(explorers, "node")
+
+        const explorers2 = result.filter(explorer => explorer.mission == "java");
+        console.log(explorers2)
+        
+        expect(result).not.toBeUndefined();
+        expect(explorers2).toStrictEqual([]);
+    });
+    test("3. Getting number of node users from explorers.json", () => {
+        const explorers = Reader.readJsonFile("./app/explorers.json");
+        const result = eService.getAmountOfExplorersByMission(explorers, "node")
+        
+        expect(result).toBe(10);
+    });
+    test("4. Getting explorers username by mission", () => {
+        const explorers = Reader.readJsonFile("./app/explorers.json");
+        const node = eService.getExplorersUsernamesByMission(explorers, "node")
+        const java = eService.getExplorersUsernamesByMission(explorers, "java")
+
+        let num = node.length
+        let num2 = java.length
+        
+        expect(num).toBe(10);
+        expect(num2).toBe(5);
+    });
+});
